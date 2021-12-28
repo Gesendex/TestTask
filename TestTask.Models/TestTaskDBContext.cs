7e73b24cd26dae39aa892a5f15041c6922e22af8
Gesendex <gireevtu@gmail.com>
2021-12-28 18:04:00 +0300

    Добавлен класс контекста бд и EF в проект

---
+++ TestTask.Models/TestTaskDBContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestTask.Models
+{
+    public class TestTaskDBContext : DbContext
+    {
+        public DbSet<Person> People { get; set; }
+        public TestTaskDBContext(DbContextOptions<TestTaskDBContext> options): base(options)
+        {
+            Database.EnsureCreated();
+        }
+    }
+}
