8a2c0a023c31675b4abe24aab7ba39f671c50b05
Gesendex <gireevtu@gmail.com>
2021-12-28 17:40:14 +0300

    Добавлен проект с моделями данных

---
+++ TestTask.Models/Person.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace TestTask.Models
+{
+    public class Person
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public string Sex { get; set; }
+    }
+}
