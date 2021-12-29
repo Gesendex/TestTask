3e5053c8e499c658d80f509c4ac634320bcf527c
Gesendex <gireevtu@gmail.com>
2021-12-30 01:08:20 +0300

    Добавлен репозиторий для работы с бд

---
+++ TestTask.Data/PeopleDBRepository.cs
@@ -0,0 +1,76 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Linq;
+using System;
+using System.Collections.Generic;
+using TestTask.Interfaces;
+using TestTask.Models;
+
+namespace TestTask.Data
+{
+    public class PeopleDBRepository : IPeopleRepository
+    {
+        private TestTaskDBContext _db;
+        public PeopleDBRepository(TestTaskDBContext db)
+        {
+            _db = db;
+        }
+        /// <summary>
+        /// Метод возвращает всех людей
+        /// </summary>
+        /// <returns>Возвращает коллекцию людей </returns>
+        public IEnumerable<Person> GetPeople()
+        {
+            return _db.People;
+        }
+        /// <summary>
+        /// Метод возвращает людей в указаном возрастном промежутке
+        /// </summary>
+        /// <param name="min">Нижняя граница возраста, передайте null если не хотите делать выборку по нижней границе</param>
+        /// <param name="max">Верхняя граница возраста, передайте null если не хотите делать выборку по верхней границе</param>
+        /// <returns>Коллекция люде в заданом возрастном промежутке</returns>
+        public IEnumerable<Person> GetPeopleByAge(int? min, int? max)
+        {
+            if (!min.HasValue && !max.HasValue)
+            {
+                return _db.People;
+            }
+            IQueryable<Person> people = _db.People;
+            if(min.HasValue)
+            {
+                people = people.Where(p => p.Age >= min);
+            }
+            if (max.HasValue)
+            {
+                people = people.Where(p => p.Age <= max);
+            }
+            return people;
+        }
+        /// <summary>
+        /// Метод возвращает людей указанного пола полу
+        /// </summary>
+        /// <param name="sex">Пол человека</param>
+        /// <returns>Коллекция людей указанного пола</returns>
+        public IEnumerable<Person> GetPeopleBySex(string sex)
+        {
+            if(sex == null)
+            {
+                return new Person[0];
+            }
+            return _db.People.Where(p => p.Sex == sex);
+
+        }
+        /// <summary>
+        /// Метод возвращает человека с заданным id
+        /// </summary>
+        /// <param name="id">id человека</param>
+        /// <returns>Возвращает человека с заданным id или если человека не существует null</returns>
+        public Person GetPersonById(string id)
+        {
+            if(id == null)
+            {
+                return null;
+            }
+            return _db.People.FirstOrDefault(p => p.Id == id);
+        }
+    }
+}
