427276df1f88794601475602dea3bb5a40d9a908
Gesendex <gireevtu@gmail.com>
2021-12-30 00:21:10 +0300

    Добавлена библиотека с интерфейсами и интерфейс для репозитория людей, а также добавлено свойство возраст в модель человека

---
+++ TestTask.Interfaces/IPeopleRepository.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using TestTask.Models;
+
+namespace TestTask.Interfaces
+{
+    public interface IPeopleRepository
+    {
+        public IEnumerable<Person> GetPeople();
+        public Person GetPersonById(string id);
+        public IEnumerable<Person> GetPeopleBySex(string sex);
+        public IEnumerable<Person> GetPeopleByAge(int? min, int? max);
+    }
+}
--- TestTask.Models/Person.cs
+++ TestTask.Models/Person.cs
@@ -7,5 +7,6 @@ namespace TestTask.Models
         public string Id { get; set; }
         public string Name { get; set; }
         public string Sex { get; set; }
+        public int Age { get; set; }
     }
 }
