b5c5780aa747397a5b3454618677ef14c743a08e
Gesendex <gireevtu@gmail.com>
2022-01-01 16:45:46 +0300

    Добавлен интерфейс PeopleService

--- TestTask.Api/Controllers/PeopleController.cs
+++ TestTask.Api/Controllers/PeopleController.cs
@@ -14,28 +14,18 @@ namespace TestTask.Api.Controllers
     public class PeopleController : ControllerBase
     {
         private readonly IPeopleRepository _peopleRepository;
-        public PeopleController(IPeopleRepository peopleRepository)
-        {
-            _peopleRepository = peopleRepository;
-        }
+        
         [HttpGet("GetPeople")]
-        public ActionResult<IEnumerable<Person>> GetPeople(string sex, int? x, int? y, int start = 0, int count = 50)
+        public ActionResult<IEnumerable<Person>> GetPeople(string sex, int x = -1, int y = -1, int start = 0, int count = 50)
         {
-            var people = _peopleRepository.GetPeople();
+            
 
-            if(!string.IsNullOrWhiteSpace(sex))
-            {
-                people = people.Where(p => p.Sex == sex);
-            }
-
-            return Ok(people);//TODO: Нужно вернуть только id, name и sex, а age не выводить
+            return Ok();//TODO: Нужно вернуть только id, name и sex, а age не выводить
         }
         [HttpGet("GetPersonById/{id}")]
         public ActionResult<Person> GetPersonById(string id)
         {
-            var person = _peopleRepository.GetPersonById(id);
-
-            return Ok(person);
+            return Ok();
         }
     }
 }
---
+++ TestTask.Interfaces/IPeopleService.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TestTask.Models;
+
+namespace TestTask.Interfaces
+{
+    interface IPeopleService
+    {
+        public IQueryable<Person> GetPeople(string sex = null, int x = -1, int y = -1, int count = 50, int start = 0);
+        public Person GetPerson(string id);
+    }
+}
